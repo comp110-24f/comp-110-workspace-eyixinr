191e55aadfb2174dac0abdd342e7046cbcdca0a3
Ethan Rong <eyixinr@unc.edu>
2024-08-27 21:45:50 -0400

    finished exercise 00

--- welcome.py
+++ welcome.py
@@ -5,3 +5,15 @@ __author__ = "01234567"
 print("Welcome to COMP110!")
 print("You are in for a fun adventure into programming!")
 print("<3 the COMP110 Team!")
+
+"""Practice with functions"""
+
+# Define a function
+
+
+def sum(num1: int, num2: int) -> int:
+    """Add two numbers together."""
+    return num1 + num2
+
+
+print(sum(num1=1, num2=10))
